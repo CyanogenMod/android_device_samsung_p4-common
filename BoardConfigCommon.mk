6209320ba0f90412a52dc4413257e1765fc93003
Steve Kondik <shade@chemlab.org>
2011-12-20 16:07:25 -0800

    Initial import of P4 common device configuration

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,100 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := false
+USE_CAMERA_STUB := false
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+#TARGET_HAVE_TEGRA_ERRATA_657451 := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := 
+BOARD_PAGE_SIZE := 2048
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_BOARD_BOOTLOADER_NAME := p3
+#TARGET_BOARD_INFO_FILE := device/samsung/p4-common/board-info.txt
+
+BOARD_EGL_CFG := device/samsung/p4-common/egl.cfg
+
+#BOARD_USES_HGL := true
+#BOARD_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+
+BOARD_USES_AUDIO_LEGACY := true
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_ventana
+
+# custom recovery ui
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/p4-common/recovery/recovery_ui.c
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# override recovery init.rc
+TARGET_RECOVERY_INITRC := device/samsung/p4-common/recovery/init.rc
+
+# Indicate that the board has an Internal SD Card
+BOARD_HAS_SDCARD_INTERNAL := true
+
+# device-specific extensions to the updater binary
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_ventana
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/p4-common
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 606076928
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 14472970240
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Wifi related defines
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAP_DRIVER := WEXT
+BOARD_HOSTAP_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WLAN_DEVICE_REV := bcm4330
+
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/bcm4330.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/vendor/firmware/fw_bcm4330_apsta.bin"
+WIFI_DRIVER_MODULE_NAME     :=  "dhd"
+WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+#BOARD_LIB_DUMPSTATE := libdumpstate.ventana
+
+# Use nicer font rendering
+BOARD_USE_SKIA_LCDTEXT := true
+
+# Charging Mode (LPM)
+BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/charging_mode_booting"
+
+# Don't dequeue current buffer
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
