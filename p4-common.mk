b7e4d9622dea5e27df6f13719512de09eb81175b
Prashant Somashekar <prashsomash@gmail.com>
2012-07-13 02:45:35 -0400

    network: add p2p_supplicant service, rfkill and wifi FW
    -reference:
     tuna, d2-common, https://github.com/xplodwild/android_device_asus_tf300t
    
    -FW is from ICS
    
    Change-Id: I9fbffb5cfd68accbfb03d8acabc5e2461ac6ba9d

--- p4-common.mk
+++ p4-common.mk
@@ -65,9 +65,10 @@ PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=15
 
-BOARD_WLAN_DEVICE_REV := bcm4330_b1
-
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/fw_bcm4330_b1.bin:system/vendor/firmware/fw_bcmdhd.bin \
+    $(LOCAL_PATH)/wifi/fw_bcm4330_apsta_b1.bin:system/vendor/firmware/fw_bcmdhd_p2p.bin \
+    $(LOCAL_PATH)/wifi/fw_bcm4330_apsta_b1.bin:system/vendor/firmware/fw_bcmdhd_apsta.bin
 
 PRODUCT_PACKAGES += \
         libinvensense_mpl
