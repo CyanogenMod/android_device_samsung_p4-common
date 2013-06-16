f128ffad212e89390677917e40ac7b2f4c8c998f
Prashant Somashekar <prashsomash@gmail.com>
2013-06-16 10:08:46 -0400

    p4-common: add WiFiDirectDemo

--- p4-common.mk
+++ p4-common.mk
@@ -129,7 +129,8 @@ PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory \
     power.p3 \
-    libnetcmdiface
+    libnetcmdiface \
+    WiFiDirectDemo
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
